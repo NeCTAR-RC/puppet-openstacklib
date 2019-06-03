98c52ecbfee116a0f32598eaa0fdb7b88edac4d9
Andy Botting <andy@andybotting.com>
2019-06-04 09:59:31 +1000

    Backport defaults from Stein to provide pyvers
    To support services moving to Stein, the packages are based on Py3
    which require the python3- package naming.
    
    By backporting this fix (which is only depended on by Stein) we
    can support versions older than Stein also.
    
    Change-Id: I7a424881aaaf7785b43f6f9951b9f08faee6a291

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -5,4 +5,16 @@
 # This file is loaded in the params.pp of each class.
 #
 class openstacklib::defaults {
+
+  if ($::os['family'] == 'Debian') {
+    $pyvers = '3'
+    $pyver3 = '3'
+  } elsif ($::os['name'] == 'Fedora') or
+          ($::os['family'] == 'RedHat' and Integer.new($::os['release']['major']) > 7) {
+    $pyvers = '3'
+    $pyver3 = '3.6'
+  } else {
+    $pyvers = ''
+    $pyver3 = '2.7'
+  }
 }
